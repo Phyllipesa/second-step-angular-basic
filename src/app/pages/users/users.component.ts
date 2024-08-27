e3d241443af253f9615d1607ec713188853ceb30
Phyllipe <phyllipe.sjc@hotmail.com>
2024-08-27 15:37:48 -0300

    Configurando o formulario

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../_models/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
-  // users: string[] = ['Galt', 'Rearden', "d'Anconia"];
+export class UsersComponent implements OnInit {
+  
   userSelected: User | undefined;
+  userForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.userForm = this.fb.group({
+      name: ['', [Validators.required, Validators.maxLength(250)]],
+      age: ['', [Validators.required, Validators.min(112), Validators.max(110)]],
+    });
+  }
+
   users: User[] = [
     {
       name: 'Galt',
@@ -21,10 +37,18 @@ export class UsersComponent {
     {
       name: "d'Anconia",
       age: 33,
+    },
+    {
+      name: "Taggart",
+      age: 33,
     }
   ];
 
   infoUserSelected(user: User) {
     this.userSelected = user;
+  }
+
+  submitForm() {
+    this.users.push(this.userForm.value);
   }
 }
