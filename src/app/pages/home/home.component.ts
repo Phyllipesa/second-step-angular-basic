1b890eaec27c9103d6a262034bb79a0fd0017f90
Phyllipe <phyllipe.sjc@hotmail.com>
2024-08-27 16:44:52 -0300

    Refatoração a Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,17 @@
-import { Component, DoCheck, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../../_service/user.service';
+import { User } from '../../_models/user';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements DoCheck {
-
-  num: number = 0;
-
-  ngDoCheck(): void {
-    console.log("Houve uma mudança na home");
-    ;
-  }
-
-  adiciona1() {
-    this.num++;
+export class HomeComponent {
+  user: User | undefined;
+  
+  constructor(private userService: UserService) {
+    this.user = this.userService.getUser();
   }
 
 }
