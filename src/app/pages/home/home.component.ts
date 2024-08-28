7c19745b2558a0a467a4f267e23f4f95ff89675c
Phyllipe <phyllipe.sjc@hotmail.com>
2024-08-28 15:10:14 -0300

    Adicionando informações da requisição ao homeComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../_service/user.service';
 import { User } from '../../_models/user';
+import { UserGit } from '../../_models/userGit';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  user: User | undefined;
-  
-  constructor(private userService: UserService) {
-    this.user = this.userService.getUser();
+export class HomeComponent implements OnInit {
+  user: UserGit | undefined;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.getGitUser();
   }
 
+  getGitUser() {
+    this.userService.getGitUser('bitcoin').subscribe((response: UserGit) => {
+      this.user = response;
+    })
+  }
 }
